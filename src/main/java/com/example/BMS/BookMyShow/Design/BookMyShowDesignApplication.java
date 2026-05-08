80142179578177635757b73a414192fabd654cc3
AviRoy1 <avijitroy2944@gmail.com>
2026-05-08 23:31:15 +0530

    build project

---
+++ src/main/java/com/example/BMS/BookMyShow/Design/BookMyShowDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.BMS.BookMyShow.Design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyShowDesignApplication.class, args);
+	}
+
+}
